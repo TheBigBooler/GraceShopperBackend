e58a9c4de829380e0735f211d3abbade26497d18
James <>
2023-08-17 14:58:24 -0500

    api/products set up and working, created a seperate 'admin' route for administrator requests in the future

--- api/orders.js
+++ api/orders.js
@@ -3,20 +3,14 @@ const router = express.Router();
 const { requireUser } = require("./utils.js");
 
 
-//admin get order history
-router.get('/', (req, res) => {
-    res.status(200).send("get request made to /orders");
-})
+
 
 //create order 
 router.post("/", (req, res) => {
   res.status(200).send("post request made to /orders");
 });
 
-//admin update order status
-router.patch("/:orderId", (req, res) => {
-  res.status(200).send(`patch request made to /orders/${req.params}`);
-});
+
 
 //export the routes!
 module.exports = router;
