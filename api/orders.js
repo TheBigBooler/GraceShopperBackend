3962a14ab828625671ecc8c41be9d63e4b40b7cf
James <>
2023-08-18 16:36:30 -0500

    order creation function succesful, returns the created order to display to user, and also subtracts the purchased quantity from the products inventory

--- api/orders.js
+++ api/orders.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { requireUser } = require("./utils.js");
-
-
+const { createOrder } = require("../db/orders.js")
+const { clearCart } = require("../db/cart.js")
 
 
 //create order 
-router.post("/", (req, res) => {
-  res.status(200).send("post request made to /orders");
+router.post("/", requireUser, async (req, res, next) => {
+  const userId = req.user.id
+  const products = req.body.products
+  try {
+    const order = await createOrder(userId, products)
+    if (!order) {
+      next({
+        name: "OrderError",
+        message: "Order was unable to be created, please try again"
+      })
+    } else if (!products) {
+      next({
+        name: "OrderError",
+        message: "Order can't be created without any products"
+      })
+    } else {
+      await clearCart(userId)
+      res.status(200).send(order)
+    }
+  } catch ({name, message}) {
+    next({name, message})
+  }
 });
 
 
